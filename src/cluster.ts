6fdd1eb46b754984c293e78787ff1d1c0a348b65
Kevin Durbin <kevindurb@gmail.com>
2021-03-12 08:26:59 -0700

    initial commit

---
+++ src/cluster.ts
@@ -0,0 +1,19 @@
+import cluster from 'cluster';
+import os from 'os';
+
+if (cluster.isMaster) {
+  const cpus = os.cpus().length;
+
+  for (let i = 0; i < cpus; i++) {
+    console.log(`Forking ${i}`);
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code) => {
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      cluster.fork();
+    }
+  });
+} else {
+  require('./');
+}
